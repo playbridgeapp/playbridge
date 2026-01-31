703df38bc808dcab20653b83ff5ad9e2cfe5a789
playbridgeapp <playbridgeapp@gmail.com>
2026-01-31 10:01:35 -0330

    Add phone project

--- phone/app/build.gradle.kts
+++ phone/app/build.gradle.kts
@@ -4,18 +4,19 @@ plugins {
 }
 
 android {
-    namespace = "com.playbridge.receiver"
+    namespace = "com.playbridge.sender"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.playbridge.receiver"
-        minSdk = 23
+        applicationId = "com.playbridge.sender"
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -38,15 +39,16 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.tv.foundation)
-    implementation(libs.androidx.tv.material)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
