ad601ccd8c159a03834ebb2a4f9e823f6472aefc
playbridgeapp <playbridgeapp@gmail.com>
2026-03-27 11:53:08 -0230

    uprev

--- tv/browser/app/build.gradle.kts
+++ tv/browser/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.playbridge.browser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.5"
+        versionCode = 6
+        versionName = "0.1.6"
     }
 
     signingConfigs {
