0f9d61171c3777bc28eb4f4e1461d88f534140c9
playbridgeapp <playbridgeapp@gmail.com>
2026-04-18 06:09:56 -0230

    add apple tv project

---
+++ tv/apple-tv/PlayBridge TV/PlayBridge TV/PlayBridge_TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayBridge_TVApp.swift
+//  PlayBridge TV
+//
+//  Created by Atul Mehla on 2026-04-18.
+//
+
+import SwiftUI
+
+@main
+struct PlayBridge_TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
