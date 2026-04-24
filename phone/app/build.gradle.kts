7c63932d7f3c767da0431adc7c6067cb48f7d41f
playbridgeapp <playbridgeapp@gmail.com>
2026-04-24 18:11:57 -0230

    chore: uprev phone 0.1.36

--- phone/app/build.gradle.kts
+++ phone/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.playbridge.sender"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
-        versionName = "0.1.35"
+        versionCode = 36
+        versionName = "0.1.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
