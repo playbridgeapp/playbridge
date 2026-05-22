0293e529fcf0ead3a6e6899351b70d6fe61dfb1a
playbridgeapp <playbridgeapp@gmail.com>
2026-05-21 23:15:35 -0230

    chore: uprev tv browser version to 0.1.24

--- tv/android/browser/app/build.gradle.kts
+++ tv/android/browser/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.playbridge.browser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.1.23"
+        versionCode = 24
+        versionName = "0.1.24"
     }
 
     testOptions {
