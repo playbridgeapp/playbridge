df5f5591bf6a39b99ca64e45336d8d9e2a8b7788
playbridgeapp <playbridgeapp@gmail.com>
2026-06-15 08:03:07 -0230

    chore: uprev phone to 0.2.7, tv-player to 0.2.8

--- tv/android/player/app/build.gradle.kts
+++ tv/android/player/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.playbridge.player"
         minSdk = 26
         targetSdk = 36
-        versionCode = 207
-        versionName = "0.2.7"
+        versionCode = 208
+        versionName = "0.2.8"
 
         ndk {
             abiFilters.add("armeabi-v7a")
